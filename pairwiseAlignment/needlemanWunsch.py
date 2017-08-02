016cba1fd75d3060264e487f0b170e29463f2236
Ben Andrew <benjamin.andrew@duke.edu>
2017-08-02 14:14:13 -0400

    Added sequence match traceback. Altered order of maximum score values to match online source code.

--- pairwiseAlignment/needlemanWunsch.py
+++ pairwiseAlignment/needlemanWunsch.py
@@ -51,31 +51,90 @@ T = pd.DataFrame(emptyT, columns=colLabs, index=rowLabs)
 # the cell whose F score it depends on
 T.iloc[0, 0] = startDot
 for i in range(1, T.shape[0]):
-    T.iloc[i, 0] = upArrow
+    T.iloc[i, 0] = trace['up']
 for j in range(1, T.shape[1]):
-    T.iloc[0, j] = leftArrow
+    T.iloc[0, j] = trace['left']
 
 # Fill the F matrix with the following rules:
 # F(i, j) = max of:
 # 1. F(i - 1, j - 1) + s(ci, cj) where s(ci, cj) is the substitution score
 # 2. F(i - 1, j) - d
 # 3. F(i, j - 1) - d
+# Gap extend penalty (e) used in place of d if more than one adjacent gap
 
 # Concurrently fill the T matrix based on which score defines the current cell
 # of the F matrix
 
+# Note that the default in the case of ties is to take the left score, then
+# diag, then up (this is the default in the source code) -- changes to this
+# order will change the final sequence match
+
+d = 8
+e = 8
+
 for i in range(1, F.shape[0]):
     for j in range(1, F.shape[1]):
+
         subPenalty = blosum50[F.index[i]][F.columns[j]]
         diagScore = F.iloc[i - 1, j - 1] + subPenalty
-        leftScore = F.iloc[i, j - 1] - d
-        upScore = F.iloc[i - 1, j] - d
+
+        if T.iloc[i, j - 1] == trace['up']:
+            leftScore = F.iloc[i, j - 1] - e
+        else:
+            leftScore = F.iloc[i, j - 1] - d
+
+        if T.iloc[i - 1, j] == trace['left']:
+            upScore = F.iloc[i - 1, j] - e
+        else:
+            upScore = F.iloc[i - 1, j] - d
+
         maxScore = max(diagScore, leftScore, upScore)
+
         F.iloc[i, j] = maxScore
-        if maxScore == diagScore:
+
+        if maxScore == leftScore:
+            T.iloc[i, j] = trace['left']
+        elif maxScore == diagScore:
             T.iloc[i, j] = trace['diag']
         elif maxScore == upScore:
             T.iloc[i, j] = trace['up']
-        elif maxScore == leftScore:
-            T.iloc[i, j] = trace['left']
 
+
+print("F matrix:")
+print(F)
+
+print("T matrix:")
+print(T)
+
+# Define alignment utilizing the T matrix traceback
+
+i = T.shape[0] - 1
+j = T.shape[1] - 1
+
+alignB = []
+alignA = []
+
+while (i, j) != (0, 0):
+    if T.iloc[i, j] == trace['diag']:
+        alignB.insert(0, T.index[i])
+        alignA.insert(0, T.columns[j])
+        i = i - 1
+        j = j - 1
+    elif T.iloc[i, j] == trace['up']:
+        alignB.insert(0, T.index[i])
+        alignA.insert(0, "-")
+        i = i - 1
+        j = j
+    elif T.iloc[i, j] == trace['left']:
+        alignB.insert(0, "-")
+        alignA.insert(0, T.columns[j])
+        i = i
+        j = j - 1
+
+print("Pairwise sequence match:")
+
+print("".join(alignA))
+print("".join(alignB))
+
+print("Final alignment score is " +
+      str(F.iloc[F.shape[0] - 1, F.shape[1] - 1]) + ".")
